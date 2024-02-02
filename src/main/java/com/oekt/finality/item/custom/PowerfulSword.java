f296fffb6a3c7b42720f49871fc76136022efc28
OE-KT <brookskentaro@gmail.com>
2024-02-01 22:00:55 -0600

    Fixed entity not dying correctly

--- src/main/java/com/oekt/finality/item/custom/PowerfulSword.java
+++ src/main/java/com/oekt/finality/item/custom/PowerfulSword.java
@@ -2,6 +2,8 @@ package com.oekt.finality.item.custom;
 
 import com.mojang.math.Quaternion;
 import com.mojang.math.Vector3f;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
@@ -9,7 +11,12 @@ import net.minecraft.world.entity.projectile.Arrow;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class PowerfulSword extends SwordItem {
     public PowerfulSword(Tier p_43269_, int p_43270_, float p_43271_, Properties p_43272_) {
@@ -19,7 +26,7 @@ public class PowerfulSword extends SwordItem {
     @Override
     public boolean hurtEnemy(ItemStack stack, LivingEntity entity, LivingEntity livingEntity) {
         entity.setHealth(0);
-
+        entity.die(DamageSource.playerAttack((Player) livingEntity));
         return false;
     }
 
