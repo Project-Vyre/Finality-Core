cbdf758e76afaac974c906202a92b332f664a7aa
OE-KT <brookskentaro@gmail.com>
2024-03-03 19:15:10 -0600

    updated dependencies

--- src/main/java/com/oekt/finality/item/custom/PowerfulSword.java
+++ src/main/java/com/oekt/finality/item/custom/PowerfulSword.java
@@ -1,7 +1,6 @@
 package com.oekt.finality.item.custom;
 
-import com.mojang.math.Quaternion;
-import com.mojang.math.Vector3f;
+
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
